0346e370e1f0116991dfeaa68859213d13485070
MasoudRahimi1991 <masoud.rahimi1991@yahoo.com>
2026-06-20 11:51:23 +0200

    Change SSL configuration for database connection

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false
+      ? { rejectUnauthorized: true }
+      : true
 });
 
 const allowedOrigins = [
